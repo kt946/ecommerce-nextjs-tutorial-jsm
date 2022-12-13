9b0c17be6b5cb7b2ed6524bd7b0e637eedec1e1d
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-12-13 17:58:55 -0500

    updated babelrc, added components, and updated homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 export default function Home() {
   return (
     <>
-      HeroBanner
+      <HeroBanner/>
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -14,7 +15,7 @@ export default function Home() {
         {['Product 1', 'Product 2'].map((product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
-  )
-}
+  );
+};
